316616cc452ffcd29a5403c7fe15c2b676149158
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-05-17 15:13:01 -0400

    App initialization

---
+++ AppetizerHub/AppetizerHub/AppetizerHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerHubApp.swift
+//  AppetizerHub
+//
+//  Created by Jeffrey Sweeney on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
